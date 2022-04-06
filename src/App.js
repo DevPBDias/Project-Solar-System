f1500d2fdeeb3b90c0fd41e46eabcefd54bfd0b5
devpbdias <devpbdias@gmail.com>
2022-04-06 16:39:49 -0300

    Requsito 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends Component {
   render() {
     return (
       <main>
         <Header />
-        <Title headline="Planetas" />
         <SolarSystem />
       </main>
     );
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import PlanetCard from './PlanetCard';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        <PlanetCard planetName="" planetImage="" />
       </div>
     );
   }
