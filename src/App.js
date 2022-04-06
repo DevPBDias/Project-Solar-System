1076171c39d4a310bd9cf965af75e2fcb3a75644
devpbdias <devpbdias@gmail.com>
2022-04-06 16:05:11 -0300

    Requsitos 2,3,4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <main>
         <Header />
+        <Title headline="Planetas" />
+        <SolarSystem />
       </main>
     );
   }
